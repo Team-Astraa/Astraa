66397de8e8abd666498e01479302a7dcc25bd9f0
prathmesh <prathameshk990@gmail.com>
2024-11-14 21:54:53 +0530

    Added Basic Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-
-import './App.css'
+import "./App.css";
 import { Badge } from "flowbite-react";
+import SignupForm from "./Pages/Signup/SignUp";
 
 function App() {
-
-
   return (
     <>
-     <h1 className='t text-center text-lg'>hii babe</h1>
-     <Badge color="info">Default</Badge>
+      <h1 className="t text-center text-lg">hii babe</h1>
+      <Badge color="info">Default</Badge>
+      <SignupForm />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
