9732bc5dc43da18acbcc7bd1677f993642c1f9cb
Varad Dhumale <varaddhumale177@gmail.com>
2024-11-15 19:00:47 +0530

    conflict reolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,27 @@
-// import "./App.css";
-// // import { Badge } from "flowbite-react";
-// import SignupForm from "./Pages/SignUp";
-// import { Toaster } from "react-hot-toast";
 
-// function App() {
-//   return (
-//     <>
-//       <Toaster position="top-right" reverseOrder={false} />
-//       <h1 className=" text-center text-2xl font-bold">
-//         WELCOME TO SIGNUP PAGE
-//       </h1>
-
-//       <SignupForm />
-//     </>
-//   );
-// }
-
-// export default App;
 
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignupForm from "./Pages/SignUp";
-import HomePage from "./Pages/Home"; // Example additional page
-import Admin from "./Pages/Admin";
 import LoginForm from "./Pages/SignIn";
+import HomePage from "./Pages/HomePage";
+import AdminHome from "./Pages/AdminHome";
+import Adminverifyuser from "./Pages/Admin-verify-user";
 // import NotFoundPage from "./Pages/NotFoundPage"; // 404 page
 
 function App() {
   return (
     <Router>
-      <Toaster position="top-right" reverseOrder={false} />
-      <h1 className="text-center text-2xl font-bold ">WELCOME TO OUR APP</h1>
       <Routes>
         {/* Define Routes for pages */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/signUp" element={<SignupForm />} />
+        <Route path="/signup" element={<SignupForm />} />
         <Route path="/signIn" element={<LoginForm />} />
-        <Route path="/Admin" element={<Admin />} />
+        <Route path="/admin/home" element={<AdminHome />} />
+        <Route path="/admin/unverify-user" element={<Adminverifyuser />} />
+
       </Routes>
+
     </Router>
   );
 }
