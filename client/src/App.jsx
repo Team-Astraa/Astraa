8b9472cf8e1dff4d807687501382c3b26526edb7
Varad Dhumale <varaddhumale177@gmail.com>
2024-11-15 16:37:18 +0530

    toast added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,21 +22,21 @@
 
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
 import SignupForm from "./Pages/SignUp";
 import HomePage from "./Pages/HomePage"; // Example additional page
-import NotFoundPage from "./Pages/NotFoundPage"; // 404 page
+import AdminHome from "./Pages/AdminHome";
 
 function App() {
   return (
     <Router>
-      <Toaster position="top-right" reverseOrder={false} />
+
       <h1 className="text-center text-2xl font-bold">WELCOME TO OUR APP</h1>
       <Routes>
         {/* Define Routes for pages */}
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupForm />} />
-        <Route path="/Admin"  element={<Admin/>}/>
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/Admin/home" element={<AdminHome />} />
       </Routes>
     </Router>
   );
