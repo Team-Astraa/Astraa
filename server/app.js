26850eb2576030f82f7ea4f319e8fe75178f3489
Sneha Deshmukh <deshmusn@gmail.com>
2024-11-17 14:57:16 +0530

    something

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ import { nanoid } from "nanoid";
 import { login, signUp } from "./controller/authController.js";
 import admin from "firebase-admin";
 import { assert } from "console";
-import serviceAccountKey from "./medium-clone-2b0eb-firebase-adminsdk-4m109-6a21350bd0.json" assert { type: "json" };
+import serviceAccountKey from "./medium-clone-2b0eb-firebase-adminsdk-4m109-6a21350bd0.json" with { type: "json" };
 import fs from 'fs';
 import path from 'path';
 import {
@@ -29,8 +29,8 @@ app.use(cors());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGODB_URI) // Use environment variable for MongoDB URI
-  // .connect("mongodb+srv://varad:varad6862@cluster0.0suvvd6.mongodb.net/SIH")
+  // .connect(process.env.MONGODB_URI) // Use environment variable for MongoDB URI
+  .connect("mongodb+srv://varad:varad6862@cluster0.0suvvd6.mongodb.net/SIH")
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
