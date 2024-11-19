13c8104f691e9168d3ca2eab25c079f2430f93b9
gvaibhav2707 <112776932+gvaibhav2707@users.noreply.github.com>
2024-11-19 12:29:52 +0530

    Responsive

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -3,15 +3,15 @@ import logo from '../assets/navlogo.png'
 
 const NavBar = () => {
     return (
-    <div className="fixed top-0 w-full flex items-center gap-0 bg-[#0f123f] h-[10vh] border-b-2 border-[#436ec677] z-40">
+    <div className="fixed top-0 w-full lg:w-[calc(100%-200px)] flex items-center bg-[#0f123f] h-[10vh] border-b-2 border-[#436ec677] z-40">
         {/* Title Section (Centered) */}
-        <div className="flex-grow flex justify-center ml-14">
+        <div className="flex-grow ml-14 lg:w-1/2 flex justify-center">
             <h1 className="text-2xl lg:text-3xl  font-bold text-white">
-                Fish Catch repo
+                Fish Catch Repo
             </h1>
         </div>
         {/* Logo Section */}
-        <div className="flex-shrink-flex-shrink-0 mx-4">
+        <div className="sm:flex-shrink-0 lg:w-1/2 p-5 flex lg:justify-center">
             <img className="w-[100px] sm:w-[80px] lg:w-[150px] h-auto lg:p-5 sm:p-0" src={logo} alt="logo" />
         </div>
     </div>
